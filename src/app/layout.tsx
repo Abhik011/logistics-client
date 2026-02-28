b8a8baeff4217c467ed47d63b391bb6930d0eea7
Abhijeet kulkarni <ak@Abhijeets-MacBook-Air.local>
2026-02-28 06:38:34 +0530

    ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,20 @@
 import "./globals.css";
+import Script from "next/script";
 
 export default function RootLayout({ children }: any) {
   return (
-    <html>
-      <body>{children}</body>
+    <html lang="en">
+      <body>
+
+        {/* 🔥 Google Maps Script */}
+        <Script
+          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY}&libraries=places`}
+          strategy="afterInteractive"
+        />
+
+        {children}
+
+      </body>
     </html>
   );
 }
